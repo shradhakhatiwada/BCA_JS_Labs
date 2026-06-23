5039bf67f5b79a86b1ecdb820612472fa322ce1f
Krishala Khatiwada <krishalakhatiwoda1999@gmail.com>
2026-06-23 10:55:17 +0545

    js folder struct

---
+++ PHP/eg2.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO 'Hello World!<br>';
+echo 'Hello World!<br>';
+
+
+
+// PHP Case Sensitivity
+// PHP keywords (e.g. if, else, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
+
+// In the example below, both echo statements are legal:
+?>
+
+</body>
+</html>
---
+++ PHP/first.php
@@ -0,0 +1,24 @@
+
+<?php
+echo 'My first PHP script!';
+
+
+#What is a PHP File?
+#PHP files have the file extension .php
+#PHP files can contain text, HTML, CSS, JavaScript, and PHP code
+#PHP code is executed on the server, and the result is returned to the browser as HTML
+
+
+
+
+// Basic PHP Syntax
+// A PHP script is executed on the server, and the plain HTML result is sent back to the browser.
+
+// A PHP script can be placed anywhere in the document.
+
+// A PHP script starts with <?php and ends with ?>:
+ 
+?> 
+
+
+
